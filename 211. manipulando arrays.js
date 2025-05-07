d1c31a4f12f601ea1f9f2c6fd4d86bc18a9cb872
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-05-07 14:51:29 -0300

    Aula 211. Manipulando Arrays

---
+++ 211. manipulando arrays.js
@@ -0,0 +1,23 @@
+const casa = [
+    'cadeira',
+    'sofa',
+    'porta',
+    'mesa',
+    'caque',
+    'quadros',
+    'aparador'
+];
+
+casa.push('cama')
+
+casa.unshift('guarda-roupa')
+casa.shift()
+
+
+const intemRemovido = casa.shift()
+
+console.log(intemRemovido)
+
+casa.forEach((intem) => {
+    console.log(intem)
+})
