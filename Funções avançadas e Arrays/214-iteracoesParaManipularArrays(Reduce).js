4344ddc93784288ab25128db00ea4f24556bd834
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-05-10 15:24:03 -0300

    Aula sobre reduce finalizada

--- Funções avançadas e Arrays/214-iteracoesParaManipularArrays(Reduce).js
+++ Funções avançadas e Arrays/214-iteracoesParaManipularArrays(Reduce).js
@@ -14,3 +14,17 @@ const valorCasa = casa.reduce((valorPrevio, valorTotal) => {
 console.log(valorCasa)
 
 // É possível fazer isso com for, for of, porém essa é uma forma mais simples e concisa
+
+const casa1 = [
+    {name: 'cadeira', preço: 50},
+    {name: 'tapete', preço: 15},
+    {name: 'sofá', preço: 150},
+    {name: 'raque', preço: 75},
+];
+
+const descricaoCasa = casa1.reduce((perv, next) => {
+    if (!perv.length) return next.name
+    return `${perv}, ${next.name}`
+}, '')
+
+console.log(descricaoCasa)
