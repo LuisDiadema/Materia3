d2962384c99464884806b0340e672ea874ad7317
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-05-18 10:57:55 -0300

    Aula 225 - Async e Await

--- Promises/225-asyncAwait.js
+++ Promises/225-asyncAwait.js
@@ -2,8 +2,6 @@
 // const buscarDado = async () => new Promise(...);
 // const buscaConcluida = await buscarDado();
 
-
-
 console.log('-- Bem-vindo ao DB --')
 
 let dataBaseSearch = async() => new Promise ((resolve, reject) => {
