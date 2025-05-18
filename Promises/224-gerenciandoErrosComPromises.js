54aaa0a13e9fcbafb766fcaf722c026ed9282afb
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-05-18 09:24:35 -0300

    Aula 224 - Gerenciamento de erro em Promises com .catch

--- Promises/224-gerenciandoErrosComPromises.js
+++ Promises/224-gerenciandoErrosComPromises.js
@@ -1,3 +1,34 @@
+// Sintaxe de uma básica de uma Promise.
+
 // Promise()
 //     .then(() => faz alguma coisa)
 //     .catch((error) => lidar com esse error)
+
+// Tratando erros em Promises com .catch.
+
+console.log('-- Bem-vindo ao DB --')
+
+let dataBaseSearch = new Promise ((resolve, reject) => {
+    console.log('Iniciando busca de Dados: ./')
+
+    setTimeout(() => {
+    let search = false;
+
+    if (search) resolve ({ Nome: 'Eustaquio', age: 65 })
+        else reject ('Infelizmente não foi possível localiza os dados')
+    }, 1000)
+})
+
+console.log('Aguarde a busca sera concluida em 5 segundo')
+
+dataBaseSearch
+    .then((dataBaseSearch) => {
+        console.log(dataBaseSearch)
+    })
+    
+// Erro sendo tratato no bloco de codigo abaixo, caso a operação não retorne o esperado
+// Uma mensagem é enviada ao úsuario.
+    .catch((error) => {
+        console.log(error)
+        console.log('Gostaria de tentar novamente ?')
+    })
