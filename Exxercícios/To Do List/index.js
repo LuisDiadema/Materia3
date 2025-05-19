7daf48f54e0be1e0094d11de3fb4f5d34dd5db69
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-05-19 17:29:31 -0300

    Estilizando layout, adicionar novas tarefas e adicionando campo de inserir novas tarefas

--- Exxercícios/To Do List/index.js
+++ Exxercícios/To Do List/index.js
@@ -4,6 +4,18 @@ let tasks = [
     {id: 3, description: 'fazer comida', checked: false},
 ]
 
+const createTaskListItem = (task, checkbox) => {
+
+    const list = document.getElementById('toDoList');
+    const toDo = document.createElement('li');
+    
+    toDo.id = task.id;
+    toDo.appendChild(checkbox);
+    list.appendChild(toDo);
+
+    return toDo;
+}
+
 const getCheckboxInput = ({id, description, checked})  => {
     const checkbox = document.createElement('input');
     const label = document.createElement('label');
@@ -12,7 +24,7 @@ const getCheckboxInput = ({id, description, checked})  => {
 
     checkbox.type = 'checkbox';
     checkbox.id = checkboxId;
-    checkbox.checked = checked;
+    checkbox.checked = checked || false;
 
     label.textContent = description;
     label.htmlFor = checkboxId;
@@ -25,7 +37,35 @@ const getCheckboxInput = ({id, description, checked})  => {
     return wrapper;
 }
 
+const getNewTaskId = () => {
+    const lastId = tasks[ tasks.length - 1 ] ?.id;
+    return lastId ? lastId + 1 : 1;
+}
+
+const getNewTaskData = (event) => {
+    const description = event.target.elements.description.value;
+    const id = getNewTaskId();
+
+    return { description, id }
+}
+
+const createTask = (event) => {
+    event.preventDefault();
+    const newTaskData = getNewTaskData(event);
+
+    const checkbox = getCheckboxInput(newTaskData);
+    createTaskListItem(newTaskData, checkbox);
+
+    tasks = [
+        ...task, 
+        { id: newTaskData.id, description: newTaskData.description, checked: false }
+    ]
+}
+
 window.onload = function () {
+    const form = document.getElementById('createToDoForm');
+    form.addEventListener('submit', createTask)
+
     tasks.forEach((task) => {
         const checkbox = getCheckboxInput(task);
 
